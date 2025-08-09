cc44faa0b9933b84ec8cee0116b5d9b42c60cdf7
Firnaz Luztian <firnazluztian@gmail.com>
2025-08-09 10:11:54 +0700

    fix: error github

--- dist/components/atoms/Input/Input.stories.tsx
+++ dist/components/atoms/Input/Input.stories.tsx
@@ -177,36 +177,6 @@ export const States: Story = {
   ),
 };
 
-// ReadOnly Examples
-export const ReadOnlyExamples: Story = {
-  render: () => (
-    <div className="flex flex-col gap-4">
-      <Input
-        label="Generated URL"
-        value="https://example.com/share/abc123"
-        readOnly
-        rightIcon="mdi:open-in-new"
-        onRightIconClick={() => alert('Opening URL in new tab...')}
-        helperText="Click the icon to open the URL"
-      />
-      <Input
-        label="Reference Number"
-        value="REF-2024-001"
-        readOnly
-        variant="success"
-        helperText="This is your unique reference number"
-      />
-      <Input
-        label="Error Message"
-        value="Invalid credentials"
-        readOnly
-        variant="error"
-        errorText="This is an error message that cannot be modified"
-      />
-    </div>
-  ),
-};
-
 // Layout Examples
 export const LayoutExamples: Story = {
   render: () => (
